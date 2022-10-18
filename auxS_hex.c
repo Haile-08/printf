06f465a071379203913a8db0e449a44a5c1330f4
Haile-08 <haile4cmd@gmail.com>
2022-10-18 14:30:41 +0300

    Create the auxs_hex file

---
+++ auxS_hex.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * auxS_hex - auxiliary to convert (S)
+ * special ascii chars to hex code
+ * 
+ * @n: number to be converted to hex
+ * Return: length
+ */
+
+int auxS_hex(int n)
+{
+
+}
