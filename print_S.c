62d1f7af39caa5990f37c3b2fea3ec4da9197aab
Haile-08 <haile4cmd@gmail.com>
2022-10-18 14:33:34 +0300

    Create the print_S file

---
+++ print_S.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_S - conversion specifier S
+ * 
+ * Description: print non-printable characters
+ * (0 < ASCII value < 32 or >= 127)
+ * /x followed by ASCII code in hex
+ * 
+ * @p: argument pointer
+ * Return: length of the string
+*/
+
+int print_S(va_list p)
+{
+    
+}
