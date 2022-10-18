143323f929b70382b9901791703753582d4e8947
Haile-08 <haile4cmd@gmail.com>
2022-10-18 15:20:06 +0300

    Finished the print_s.c file

--- print_S.c
+++ print_S.c
@@ -13,5 +13,20 @@
 
 int print_S(va_list p)
 {
+    int i = 0, c = 0;
+    char *ptr_str = va_arg(p, char*);
 
+    if (ptr_str == NULL)
+        return (write(1, "(null)", 6));
+    while (ptr_str[i] != '\0')
+    {
+        if (ptr_str[i] >= 32 && ptr_str[i] < 127)
+            c += _putchar(ptr_str[i]);
+        else
+        {
+            c += auxS_hex(ptr_str[i]);
+        }
+        i++;
+    }
+    return (c);
 }
